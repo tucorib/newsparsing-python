0da6e5c677982add60226c885422d9c8b1b04af4
Nicolas Ribeiro <ribeiro@safety-data.com>
2018-01-03 00:49:26 +0100

    Fix logging

--- services/libs/newsparsing-common/src/newsparsing/common/configuration.py
+++ services/libs/newsparsing-common/src/newsparsing/common/configuration.py
@@ -4,7 +4,7 @@ Created on 2 janv. 2018
 @author: tuco
 '''
 from pyhocon.config_parser import ConfigFactory
-import logging
+import logging.config
 
 # Configuration
 configuration = None
@@ -15,7 +15,7 @@ def load(configuration_path):
     configuration = ConfigFactory.parse_file(configuration_path)
     
     if configuration.get('loggers.articles'):
-        logging.config.fromConfig(configuration.get('loggers.articles'))
+        logging.config.fileConfig(configuration.get('loggers.articles'))
         logger = logging.getLogger('articles')
         logger.debug('Loaded: %s' % configuration.get('loggers.articles'))
 
