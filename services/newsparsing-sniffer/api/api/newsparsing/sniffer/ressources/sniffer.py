081c3891aa2b65e89a0dc538f115225f4426aaeb
Nicolas Ribeiro <ribeiro@safety-data.com>
2018-01-06 15:03:06 +0100

    Stream sniffer

--- services/newsparsing-sniffer/api/api/newsparsing/sniffer/ressources/sniffer.py
+++ services/newsparsing-sniffer/api/api/newsparsing/sniffer/ressources/sniffer.py
@@ -4,13 +4,30 @@ Created on 6 janv. 2018
 @author: tuco
 '''
 from flask.blueprints import Blueprint
-from flask.json import jsonify
 from newsparsing.sniffer.sniffer import sniff as sniff_src
+from flask import Response
+from flask.json import jsonify
 
 sniffer_blueprint = Blueprint('sniffer', __name__)
+
+
+def stream_json_array(iterator, field_name):
+    try:
+        prev = next(iterator)  # Get first result
+    except StopIteration:
+        # Empty iterator, return now
+        yield '{"%s": []}' % field_name
+        raise StopIteration
+    
+    yield '{"%s": [' % field_name
+    # Iterate
+    for _ in iterator:
+        yield jsonify(_) + ', '
+        prev = _
+    # Now yield the last iteration without comma but with the closing brackets
+    yield jsonify(prev) + ']}'
 
     
 @sniffer_blueprint.route('/sniff/<source_type>/<source_name>', methods=['GET'])
 def sniff(source_type, source_name):
-    articles = [_ for _ in sniff_src(source_type, source_name)]
-    return jsonify(articles), 200
+    return Response(stream_json_array(sniff_src(source_type, source_name), 'articles'), mimetype="application/json")
--- services/newsparsing-sniffer/test/test/api/sniffer/sniffer.py
+++ services/newsparsing-sniffer/test/test/api/sniffer/sniffer.py
@@ -5,6 +5,7 @@ Created on 6 janv. 2018
 '''
 from test.api import ApiTest
 import unittest
+from flask import json
 
 
 class TestSniff(unittest.TestCase, ApiTest):
@@ -20,3 +21,6 @@ class TestSniff(unittest.TestCase, ApiTest):
         response = self.client.get('/sniff/%s/%s' % (self.TEST_SOURCE_TYPE,
                                                      self.TEST_SOURCE_NAME))
         self.assertResponseCode(response, 200)
+        data = json.loads(response.data)
+        self.assertIn('articles', data, "Response does not contain 'articles'")
+        
