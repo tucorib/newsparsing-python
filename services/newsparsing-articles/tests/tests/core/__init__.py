b3168ca1758f54ec14a89b44b03b11d3dc94c44e
Nicolas Ribeiro <ribeiro@safety-data.com>
2018-01-10 20:25:17 +0100

    Update articles

--- services/newsparsing-articles/tests/tests/core/__init__.py
+++ services/newsparsing-articles/tests/tests/core/__init__.py
@@ -7,7 +7,7 @@ from tests import CONFIG_DIR
 class ArticlesTestCase():
 
     APPLICATION_CONFIGURATION = os.path.join(CONFIG_DIR,
-                                             "test.application.conf")
+                                             "tests.application.conf")
 
     def setUp(self):
         # Load configuration
